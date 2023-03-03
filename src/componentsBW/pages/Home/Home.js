1842d3d67845b18bc051cf5bd4758769b5aab1de
novatogatorop <nova@roadbeats.com>
2023-03-03 15:45:22 +0100

    update bio

--- src/componentsBW/pages/Home/Home.js
+++ src/componentsBW/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Home.css";
 import Button from "../../Button";
 import Headline from "../../Headline";
@@ -14,7 +14,8 @@ import {
   postgresql,
   react,
   swift,
-  webpack } from "./Data";
+  webpack,
+} from "./Data";
 
 class Home extends Component {
   render() {
@@ -24,10 +25,13 @@ class Home extends Component {
           Nova Togatorop — is a Full Stack Developer based in Berlin.
         </h1>
         <h1 className="home-desc">
-          I'm currently learning React and reading a book in my spare time.
+          I'm currently learning Cloud computing and reading a book in my spare
+          time.
         </h1>
         <h1 className="home-desc">
-          I'm open for collaborations. If you’re interested in discussing a project, making something great together, or simply getting in touch, email me.
+          I'm open for collaborations. If you’re interested in discussing a
+          project, making something great together, or simply getting in touch,
+          email me.
         </h1>
         <Headline {...technologies} />
         <div class="card">
@@ -42,8 +46,7 @@ class Home extends Component {
           <Card {...heroku} />
           <Card {...webpack} />
         </div>
-        <div class="info">
-        </div>
+        <div class="info"></div>
       </>
     );
   }
