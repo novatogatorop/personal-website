a28f62653cdd1ce89c0130016c2bb30415d2206a
novatogatorop <nova@roadbeats.com>
2023-03-06 16:26:01 +0100

    add experience content

--- src/componentsBW/pages/Experience/Data.js
+++ src/componentsBW/pages/Experience/Data.js
@@ -1,4 +1,61 @@
-export const experience= {
+export const experience = {
   title: "EXPERIENCE",
-  description: ""
+  description: "OVERVIEW OF MY WORK EXPERIENCE",
 };
+
+export const experiences = [
+  {
+    id: "1",
+    title: "FULL STACK ENGINEER",
+    period: "Jan 22 - Jan 23",
+    company: "WebinarGeek",
+    city: "Berlin",
+    description:
+      "Work on a team of 8 to rebuild the frontend streaming app from legacy written in Ruby on Rails to React + Typescript. Improved API endpoints and data structures while integrating the React-based frontend with the Rails backend. Working closely with product designer, brainstorm about UX, and mobile flow, continuously giving detailed input from user and technical perspective.",
+  },
+  {
+    id: "2",
+    title: "SOFTWARE ENGINEER",
+    period: "Apr 2021 - Oct 2021",
+    company: "Arbour",
+    city: "Berlin",
+    description:
+      "Work focused on software development, test coverage, User Interface improvements and bug fixes. Building and maintaining new landing page of the site.",
+  },
+  {
+    id: "3",
+    title: "FOUNDER",
+    period: "Jan 2020 - present",
+    company: "NAPPIES",
+    city: "Berlin",
+    description:
+      "Nappies is an application for finding the nearest baby friendly places wherever you want based on provided facilities. It got released in the iOS store in July 2020.",
+  },
+  {
+    id: "4",
+    title: "TEACHING ASSISTANT",
+    period: "Jan 2020 - Present",
+    company: "Le Wagon",
+    city: "Berlin",
+    description:
+      "I help students to solve technical problems and to understand the concepts of Ruby, OOP, MVC Architecture, Ruby on Rails as a teaching assistant at Le Wagon Berlin, by leading them to the solutions.",
+  },
+  {
+    id: "5",
+    title: "TEACHER/VOLUNTEER",
+    period: "May 2020 - Present",
+    company: "Re-Di School",
+    city: "Berlin",
+    description:
+      "Participate in Kids and Digital Women Program. Contribute to Coding and Web Design Fundamentals courses.",
+  },
+  {
+    id: "6",
+    title: "PHOTOGRAPHER",
+    period: "Oct  2015 - Oct 2019",
+    company: "Shuttershock",
+    city: "Oakland, CA",
+    description:
+      "My work was focused on Lifestyle, Product, Travel, and Real Estate. Completed assignments for Marriot, Wells Fargo, Manulife Asia, UBS and etc",
+  },
+];
--- src/componentsBW/pages/Experience/Experience.js
+++ src/componentsBW/pages/Experience/Experience.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Experience.css";
 import Banner from "../../Banner";
-import { experience } from "./Data"
+import { experience, experiences } from "./Data";
 
 class Experience extends Component {
   render() {
     return (
       <>
         <Banner {...experience} />
+        {experiences.map((e) => {
+          return (
+            <div key={e.id} className="experience-list">
+              <div className="listHeading">
+                <div className="title">{e.title}</div>
+                <div className="company">
+                  - {e.company}, {e.city}
+                </div>
+              </div>
+              <div className="period">{e.period}</div>
+              <div className="description">{e.description}</div>
+            </div>
+          );
+        })}
       </>
     );
   }
